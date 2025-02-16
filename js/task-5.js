8e3df5eb8257ed258d925eb638529ab2f10a8beb
Volodymyr Vladykin <vova@pocketguard.com>
2025-02-16 20:13:09 +0200

    Task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const body = document.querySelector("body");
+const colorOutput = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", (event) => {
+  const newHexColor = getRandomHexColor();
+  colorOutput.textContent = newHexColor;
+  body.style.backgroundColor = newHexColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
