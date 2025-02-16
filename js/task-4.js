d39c52278d82c419be01036fb3ef290e549e7536
Volodymyr Vladykin <vova@pocketguard.com>
2025-02-16 20:06:36 +0200

    Task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const email = event.currentTarget.elements.email.value.trim();
+  const password = event.currentTarget.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const userData = {
+    email,
+    password,
+  };
+
+  console.log(userData);
+  loginForm.reset();
+});
